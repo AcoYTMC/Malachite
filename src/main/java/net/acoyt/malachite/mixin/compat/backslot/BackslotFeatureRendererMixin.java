82d042d100379b6d437b68f7543b30010ac8a9b1
AcoYT <167049722+AcoYTMC@users.noreply.github.com>
2026-03-19 07:18:36 -0700

    WAFKJAFW

--- src/main/java/net/acoyt/malachite/mixin/compat/backslot/BackslotFeatureRendererMixin.java
+++ src/main/java/net/acoyt/malachite/mixin/compat/backslot/BackslotFeatureRendererMixin.java
@@ -2,8 +2,10 @@ package net.acoyt.malachite.mixin.compat.backslot;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.acoyt.malachite.impl.index.MalachiteItems;
 import net.medecoole.backslot.registry.BackslotFeatureRenderer;
+import net.medecoole.backslot.registry.BackslotItemComponent;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.feature.FeatureRenderer;
@@ -36,5 +38,24 @@ public abstract class BackslotFeatureRendererMixin extends FeatureRenderer<Abstr
     private void customMode(ItemRenderer instance, ItemStack stack, ModelTransformationMode transformationType, int light, int overlay, MatrixStack matrices, VertexConsumerProvider vertexConsumers, World world, int seed, Operation<Void> original) {
         ModelTransformationMode mode = stack.isOf(MalachiteItems.MALACHITE_LONGSWORD) ? ModelTransformationMode.HEAD : transformationType;
         original.call(instance, stack, mode, light, overlay, matrices, vertexConsumers,  world, seed);
+    }
+
+    @WrapOperation(
+            method = "render(Lnet/minecraft/client/util/math/MatrixStack;" +
+                    "Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/client/network/AbstractClientPlayerEntity;FFFFFF)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/util/math/MatrixStack;translate(DDD)V",
+                    ordinal = 2
+            )
+    )
+    private void imTooLazyToRemakeTheTransformation(MatrixStack instance, double x, double y, double z, Operation<Void> original, @Local(argsOnly = true) AbstractClientPlayerEntity entity) {
+        BackslotItemComponent component = BackslotItemComponent.get(entity);
+        if (component.getStack().isOf(MalachiteItems.MALACHITE_LONGSWORD)) {
+            original.call(instance, x, y, -z);
+            return;
+        }
+
+        original.call(instance, x, y, z);
     }
 }
